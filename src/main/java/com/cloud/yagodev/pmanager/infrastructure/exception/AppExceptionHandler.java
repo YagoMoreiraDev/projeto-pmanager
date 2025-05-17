76f59f4e769f5632016ea2caf3122391c37120bd
Yago Moreira <yagomoreira_dev@yahoo.com>
2025-05-16 22:04:15 -0300

    Criado, sem implementaçao ainda, a classe cujo o objeto será para tratar excecoes, classe AppExceptionHandler, alterei meu application.properties para o uso do application.yml

---
+++ src/main/java/com/cloud/yagodev/pmanager/infrastructure/exception/AppExceptionHandler.java
@@ -0,0 +1,10 @@
+package com.cloud.yagodev.pmanager.infrastructure.exception;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class AppExceptionHandler extends ResponseEntityExceptionHandler {
+
+
+}
